c565fe8d7e1e6be8929e387e077a1e7cff3f38c3
M4cs <maxlikescs@gmail.com>
2018-05-24 19:10:17 -0400

    Removed Midnights Stuff

--- HaystackCC/HSKEzCCModule.h
+++
@@ -1,8 +0,0 @@
-#import <Haystack/HSKContentModule-Protocol.h>
-#import "HSKEzCCModuleViewController.h"
-
-@interface HSKEzCCModule : NSObject <HSKContentModule> {
-	HSKEzCCModuleViewController	*_viewController;
-}
-@property(readonly, nonatomic) HSKControlCenterPage *contentViewController;
-@end
--- excitant-master/README.md
+++
@@ -1,3 +0,0 @@
-# excitant
-
-Be sure to add libexcitant (extract from the deb) to theos /lib folder
